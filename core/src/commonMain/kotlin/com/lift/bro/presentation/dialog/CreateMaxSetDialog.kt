01eaf69e3d0cf6c0eca8d5bc7ae778e806e61228
Evan Holtrop <evanholtrop@gmail.com>
2025-03-09 18:43:54 -0400

    create set max dialog

--- core/src/commonMain/kotlin/com/lift/bro/presentation/dialog/CreateMaxSetDialog.kt
+++ core/src/commonMain/kotlin/com/lift/bro/presentation/dialog/CreateMaxSetDialog.kt
@@ -6,27 +6,39 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import com.benasher44.uuid.uuid4
 import com.lift.bro.di.dependencies
 import com.lift.bro.domain.models.LBSet
 import com.lift.bro.domain.models.Tempo
+import com.lift.bro.domain.models.fullName
+import com.lift.bro.presentation.components.DropDownButton
 import com.lift.bro.presentation.spacing
+import com.lift.bro.ui.DecimalFormat
 import com.lift.bro.ui.Space
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -53,16 +65,12 @@ fun CreateMaxSetDialog(
                     shape = MaterialTheme.shapes.large,
                 ).padding(MaterialTheme.spacing.one),
             ) {
-                val parentLift by dependencies.database.liftDataSource.get(parentLiftId)
-                    .collectAsState(null)
-
-
-                val variations =
-                    dependencies.database.variantDataSource.getAll(parentLift?.id ?: "")
+                val variations = dependencies.database.variantDataSource.getAll(parentLiftId)
 
                 var selectedVariation by remember { mutableStateOf(variations.firstOrNull()) }
 
                 var weight by remember { mutableStateOf<Double?>(null) }
+                var reps by remember { mutableStateOf<Int?>(1) }
 
                 val coroutineScope = rememberCoroutineScope()
 
@@ -75,12 +83,44 @@ fun CreateMaxSetDialog(
 
                 Space(MaterialTheme.spacing.two)
 
-                parentLift?.let { lift ->
+                Row {
                     TextField(
-                        value = weight.toString(),
+                        modifier = Modifier.width(52.dp),
+                        value = reps?.toString() ?: "",
+                        onValueChange = { reps = it.toIntOrNull()},
+                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+                    )
+
+                    Space(MaterialTheme.spacing.half)
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        text = "x",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Space(MaterialTheme.spacing.half)
+
+                    TextField(
+                        modifier = Modifier.weight(1f),
+                        value = DecimalFormat.formatWeight(weight),
                         onValueChange = { weight = it.toDoubleOrNull() },
-                        suffix = {
-                            Text("${selectedVariation?.name} ${lift.name}")
+                        supportingText = {
+                            Text("weight in lbs")
+                        },
+                        trailingIcon = {
+                            DropDownButton(
+                                buttonText = selectedVariation?.fullName ?: "",
+                            ) {
+                                variations.forEach { variation ->
+                                    DropdownMenuItem(
+                                        text = {
+                                            Text(variation.fullName)
+                                        },
+                                        onClick = {
+                                            selectedVariation = variation
+                                        }
+                                    )
+                                }
+                            }
                         }
                     )
                 }
@@ -99,7 +139,7 @@ fun CreateMaxSetDialog(
                     }
                     Space(MaterialTheme.spacing.half)
                     Button(
-                        enabled = weight != null,
+                        enabled = weight != null && reps != null,
                         onClick = {
                             isLoading = true
                             coroutineScope.launch(context = Dispatchers.IO) {
@@ -109,7 +149,7 @@ fun CreateMaxSetDialog(
                                         id = uuid4().toString(),
                                         variationId = selectedVariation!!.id,
                                         weight = weight!!,
-                                        reps = 1,
+                                        reps = reps!!.toLong(),
                                         notes = ""
                                     )
                                 )
---
+++ core/src/commonMain/kotlin/com/lift/bro/utils/StringFormatting.kt
@@ -0,0 +1,5 @@
+package com.lift.bro.utils
+
+import com.lift.bro.ui.DecimalFormat
+
+fun Double?.formatWeight() = DecimalFormat.formatWeight(this)
