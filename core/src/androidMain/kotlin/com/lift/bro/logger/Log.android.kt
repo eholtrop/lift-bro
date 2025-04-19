aca50136d1e5ead1a4cae694cb758713fe4bd9a3
Evan Holtrop <evanholtrop@gmail.com>
2025-04-19 09:51:27 -0400

    Fix Refactor build failures

--- core/src/androidMain/kotlin/com/lift/bro/logger/Log.android.kt
+++
@@ -1,7 +0,0 @@
-package com.lift.bro.logger
-
-import com.lift.bro.utils.logger.Log
-
-actual fun Log.d(tag: String?, message: String) {
-    android.util.Log.d(tag, message)
-}
---
+++ core/src/androidMain/kotlin/com/lift/bro/utils/logger/Log.android.kt
@@ -0,0 +1,5 @@
+package com.lift.bro.utils.logger
+
+actual fun Log.d(tag: String?, message: String) {
+    Log.d(tag, message)
+}
