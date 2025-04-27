17880805e1f95cfefdf00cc02b23f371a33e358a
Evan Holtrop <evanholtrop@gmail.com>
2025-04-27 09:02:10 -0400

    fix: Hook up Activity OnBackPressed to Coordinator

--- androidApp/src/main/java/com/lift/bro/android/MainActivity.kt
+++ androidApp/src/main/java/com/lift/bro/android/MainActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import com.lift.bro.data.Backup
 import com.lift.bro.di.DependencyContainer
 import com.lift.bro.di.dependencies
 import com.lift.bro.presentation.App
 import com.lift.bro.presentation.StoreManager
+import com.lift.bro.ui.navigation.Destination
+import com.lift.bro.ui.navigation.rememberNavCoordinator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -28,7 +31,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            val coordinator = rememberNavCoordinator(Destination.Dashboard)
+            App(
+                navCoordinator = coordinator
+            )
+
+            BackHandler {
+                if (!coordinator.onBackPressed()) {
+                    onBackPressed()
+                }
+            }
         }
     }
 
