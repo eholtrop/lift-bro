61d17f91f311854b178129cd3ec17d06c5b2aeb5
Evan Holtrop <evanholtrop@gmail.com>
2025-05-18 10:07:36 -0400

    feat: implement date formatting for iOS

--- core/src/nativeMain/kotlin/com/lift/bro/utils/DateFormatting.native.kt
+++ core/src/nativeMain/kotlin/com/lift/bro/utils/DateFormatting.native.kt
@@ -8,17 +8,22 @@ import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toNSDate
 import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
+import platform.Foundation.currentLocale
+
+private fun dateFormatter(pattern: String) = NSDateFormatter().apply {
+    this.dateFormat = pattern
+    this.locale = NSLocale.currentLocale
+}
 
 actual fun Instant.toString(pattern: String): String {
-    return NSDateFormatter().stringFromDate(this.toNSDate())
+    return dateFormatter(pattern).stringFromDate(this.toNSDate())
 }
 
 actual fun LocalDate.toString(pattern: String): String {
-    return NSDateFormatter().apply {
-        this.dateFormat = pattern
-    }.stringFromDate(this.atStartOfDayIn(TimeZone.currentSystemDefault()).toNSDate())
+    return dateFormatter(pattern).stringFromDate(this.atStartOfDayIn(TimeZone.currentSystemDefault()).toNSDate())
 }
 
 actual fun LocalDateTime.toString(pattern: String): String {
-    return NSDateFormatter().stringFromDate(this.toInstant(TimeZone.currentSystemDefault()).toNSDate())
+    return dateFormatter(pattern).stringFromDate(this.toInstant(TimeZone.currentSystemDefault()).toNSDate())
 }
