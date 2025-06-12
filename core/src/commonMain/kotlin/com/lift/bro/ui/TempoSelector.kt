fca772a0ff1db7654e99ae5f6346af3c20c7a15d
Evan Holtrop <evanholtrop@gmail.com>
2025-06-11 22:34:33 -0400

    Replaces material dialog with infoiconbutton

--- core/src/commonMain/kotlin/com/lift/bro/ui/TempoSelector.kt
+++ core/src/commonMain/kotlin/com/lift/bro/ui/TempoSelector.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -22,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import com.lift.bro.ui.dialog.InfoDialogButton
 import com.lift.bro.ui.theme.spacing
 
 @Composable
@@ -68,16 +68,10 @@ fun TempoSelector(
                 text = "With a tempo of...",
                 style = MaterialTheme.typography.titleMedium
             )
-            IconButton(
-                onClick = {
-                    showInfoModal = true
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = "Info"
-                )
-            }
+            InfoDialogButton(
+                dialogTitle = { Text("What is Tempo?") },
+                dialogMessage = { InfoDialogText() }
+            )
         }
 
         Row(
