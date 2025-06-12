26b263554b719c89f26de6bee097b9291b8f02d9
Evan Holtrop <evanholtrop@gmail.com>
2025-06-12 16:11:50 -0400

    Improves workout calendar day marker display
    Refines the display of workout indicators on the calendar.
    Limits the number of indicators shown to four for better readability.

--- core/src/commonMain/kotlin/com/lift/bro/presentation/home/WorkoutCalendarScreen.kt
+++ core/src/commonMain/kotlin/com/lift/bro/presentation/home/WorkoutCalendarScreen.kt
@@ -18,8 +18,11 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.lift.bro.domain.models.Excercise
@@ -76,10 +80,12 @@ fun WorkoutCalendarScreen(
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.quarter)
                     ) {
-                        setDateMap[date]?.map { it.variation.lift?.color?.toColor() ?: MaterialTheme.colorScheme.primary }?.forEach { color ->
+                        setDateMap[date]?.map {
+                            it.variation.lift?.color?.toColor() ?: MaterialTheme.colorScheme.primary
+                        }?.take(4)?.forEachIndexed { index, color ->
                             Box(
                                 modifier = Modifier.background(
-                                    color = if (selectedDate == date) MaterialTheme.colorScheme.onSecondary else color,
+                                    color = color,
                                     shape = CircleShape,
                                 ).size(4.dp)
                             )
