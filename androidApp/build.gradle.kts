d4e09d7f26e8b8ab85f432484f3a78cb56de669f
Evan Holtrop <evanholtrop@gmail.com>
2025-06-22 12:56:01 -0400

    Updates date format for version name
    Corrects the date format in the version name generation
    to ensure proper date representation.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.lift.bro"
         minSdk = 24
         targetSdk = 35
-        versionName = SimpleDateFormat("dd.mm.yyyy").format(Date())
+        versionName = SimpleDateFormat("dd.MM.yyyy").format(Date())
 
         versionCode = if (project.hasProperty("buildNumber")) {
             property("buildNumber").toString().toInt() + 156
