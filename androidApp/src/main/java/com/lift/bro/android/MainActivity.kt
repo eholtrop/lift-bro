d27ea8c508ad594396717be0bd76a972eba117d4
Evan Holtrop <evanholtrop@gmail.com>
2025-07-19 15:59:49 -0400

    fix[and]: fix crash when back is pressed on the first page

--- androidApp/src/main/java/com/lift/bro/android/MainActivity.kt
+++ androidApp/src/main/java/com/lift/bro/android/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : ComponentActivity() {
             )
             BackHandler {
                 if (!coordinator.onBackPressed()) {
-                    onBackPressed()
+                    finish()
                 }
             }
         }
