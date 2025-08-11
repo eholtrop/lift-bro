84d32a793813ebf5f23d3077d9efd8c0d70bef4e
Evan Holtrop <evanholtrop@gmail.com>
2025-08-11 10:29:31 -0400

    Updates theme settings row layout
    Changes the layout of the theme settings row from a horizontal row to a vertical column.
    
    This improves the readability and organization of the theme options, especially on smaller screens.

--- core/src/commonMain/kotlin/com/lift/bro/presentation/settings/ThemeSettingsRow.kt
+++ core/src/commonMain/kotlin/com/lift/bro/presentation/settings/ThemeSettingsRow.kt
@@ -1,5 +1,6 @@
 package com.lift.bro.presentation.settings
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,7 +23,7 @@ fun ThemeSettingsRow() {
     ) {
         val themeMode by dependencies.settingsRepository.getThemeMode()
             .collectAsState(ThemeMode.System)
-        Row {
+        Column {
             RadioField(
                 text = stringResource(Res.string.settings_theme_option_one),
                 selected = themeMode == ThemeMode.System,
