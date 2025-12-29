1b291bc241adb067887e26a7a20ab49ee7b11051
eholtrop <evanholtrop@gmail.com>
2025-12-29 14:49:18 -0500

    GetVariatoinProgressUseCase: Update maths to ensure the max 1 rep is found

--- presentation/compose/src/commonMain/kotlin/com/lift/bro/presentation/wrapped/usecase/GetVariationProgressUseCase.kt
+++ presentation/compose/src/commonMain/kotlin/com/lift/bro/presentation/wrapped/usecase/GetVariationProgressUseCase.kt
@@ -40,22 +40,26 @@ class GetVariationProgressUseCase(
         sets.groupBy { set -> variations.first { it.id == set.variationId } }
             .filter { it.value.isNotEmpty() }
             .mapValues { (variation, variationSets) ->
-                val orderedSets = variationSets
+                val dateSets: List<Pair<LocalDate, List<LBSet>>> = variationSets
                     .groupBy { it.date.toLocalDate() }
                     .toList()
                     .sortedByDescending { it.first }
 
-                val minSet = when (variation.bodyWeight) {
-                    true -> orderedSets.last().second.maxBy { it.reps }
-                    // find last one rep max, if none then get the last sets max weight lifted
-                    else -> orderedSets.lastOrNull { it.second.any { it.reps == 1L } }?.second?.lastOrNull { it.reps == 1L }
-                        ?: orderedSets.last().second.maxBy { it.weight }
+                val minSet: LBSet = when (variation.bodyWeight) {
+                    true -> dateSets.last().second.maxBy { it.reps }
+                // find last one rep max
+                    else -> dateSets.lastOrNull { dateSet -> dateSet.second.any { set -> set.reps == 1L } }
+                        ?.second?.filter { it.reps == 1L }?.maxBy { it.weight }
+                    // if none then get the last sets max weight lifted
+                        ?: dateSets.last().second.maxBy { it.weight }
                 }
-                val maxSet = when (variation.bodyWeight) {
-                    true -> orderedSets.first().second.maxBy { it.reps }
-                    // find last one rep max, if none then get the last sets max weight lifted
-                    else -> orderedSets.firstOrNull { it.second.any { it.reps == 1L } }?.second?.firstOrNull { it.reps == 1L }
-                        ?: orderedSets.first().second.maxBy { it.weight }
+                val maxSet: LBSet = when (variation.bodyWeight) {
+                    true -> dateSets.first().second.maxBy { it.reps }
+                    // find first one rep max
+                    else -> dateSets.firstOrNull { dateSet -> dateSet.second.any { set -> set.reps == 1L } }
+                        ?.second?.filter { it.reps == 1L }?.maxBy { it.weight }
+                    //if none then get the first sets max weight lifted
+                        ?: dateSets.first().second.maxBy { it.weight }
                 }
 
                 VariationProgress(
