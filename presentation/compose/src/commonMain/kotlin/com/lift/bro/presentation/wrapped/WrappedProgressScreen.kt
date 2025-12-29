117d2cea673a2dcf174754e138843924de8ed790
eholtrop <evanholtrop@gmail.com>
2025-12-29 15:03:59 -0500

    WrappedProgressScreen: Remove "-" from dates

--- presentation/compose/src/commonMain/kotlin/com/lift/bro/presentation/wrapped/WrappedProgressScreen.kt
+++ presentation/compose/src/commonMain/kotlin/com/lift/bro/presentation/wrapped/WrappedProgressScreen.kt
@@ -213,7 +213,7 @@ fun ProgressItemView(
                     horizontalAlignment = Alignment.Start,
                 ) {
                     Text(
-                        text = date.toString("MMM - d"),
+                    text = date.toString("MMM d"),
                         style = MaterialTheme.typography.labelMedium,
                     )
                     Text(
