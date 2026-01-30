eefade3a824f49250a9bb4e898961c6a82d8be06
eholtrop <evanholtrop@gmail.com>
2026-01-30 12:05:00 -0500

    Move lingering date extensions into extensions

---
+++ libs/ext/ktx-datetime/src/commonMain/kotlin/tv/dpal/ktx/datetime/LocalDateExt.kt
@@ -0,0 +1,14 @@
+package tv.dpal.ktx.datetime
+
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
+
+
+fun LocalDate.atStartOfDayIn(): Instant = this.atStartOfDayIn(TimeZone.currentSystemDefault())
+
+fun LocalDate.atEndOfDayIn(): Instant =
+    this.atTime(23, 59, 59, 999).toInstant(TimeZone.currentSystemDefault())
