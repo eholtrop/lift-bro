44d513065d8753a0d369eb2d81ed244b36f64309
eholtrop <evanholtrop@gmail.com>
2026-01-30 17:32:47 -0500

    readd navi

---
+++ libs/navi/settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "navi"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
