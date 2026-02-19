37b25b2a52abfd2cd6fbecf7964ac47f52111e0a
eholtrop <evanholtrop@gmail.com>
2026-02-19 16:06:39 -0500

    show preview of camera on planning page

---
+++ presentation/compose/src/androidMain/kotlin/com/lift/bro/presentation/camera/CameraPermission.android.kt
@@ -0,0 +1,37 @@
+package com.lift.bro.presentation.camera
+
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+actual fun rememberCameraPermissionHandler(): ICameraPermissionHandler {
+
+    var hasPermission by remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        hasPermission = permissions.values.all { it }
+    }
+
+    return remember {
+        object : ICameraPermissionHandler {
+            override val isGranted: Boolean get() = hasPermission
+
+            override fun requestPermission(onResult: (Boolean) -> Unit) {
+                permissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO
+                    )
+                )
+            }
+        }
+    }
+}
