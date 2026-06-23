32520d7c9e80beb4808880d7ae24bed838019b18
Evan Holtrop <evanholtrop@gmail.com>
2026-06-23 14:46:20 -0400

    speed up ios ssh builds

--- scripts/deploy-ios-device.sh
+++ scripts/deploy-ios-device.sh
@@ -11,24 +11,35 @@ echo "Remote: $REMOTE_HOST"
 echo "Device UDID: $DEVICE_UDID"
 echo ""
 
-echo "[1/2] Syncing project to $REMOTE_HOST..."
-rsync -avz \
+# Pre-warm Gradle daemon on remote while we sync files
+echo "[1/3] Pre-warming Gradle daemon on $REMOTE_HOST..."
+ssh "eholtrop@$REMOTE_HOST" \
+  "cd $REMOTE_PATH && ./gradlew --stop > /dev/null 2>&1; ./gradlew -q --build-cache :presentation:compose:dependencies > /dev/null 2>&1 &"
+
+echo "[2/3] Syncing project to $REMOTE_HOST..."
+rsync -a --info=progress2 \
+  --delete \
   --exclude '.git' \
   --exclude '.idea/' \
   --exclude '.gradle/' \
   --exclude 'build/' \
+  --exclude 'fastlane/build/' \
   --exclude '*.iml' \
   --exclude 'DerivedData/' \
   --exclude 'iosApp/build/' \
+  --exclude 'node_modules/' \
+  --exclude 'marketing-website/' \
+  --exclude '.maestro/' \
+  --exclude 'maestro_tests/' \
+  --exclude 'hooks/' \
   --exclude 'iosApp/iosApp/GoogleService-Info.plist' \
   --exclude 'iosApp/Configuration.storekit' \
   --exclude 'iosApp/StoreKitTestCertificate.cer' \
   . "eholtrop@$REMOTE_HOST:$REMOTE_PATH/"
 
 echo ""
-echo "[2/2] Building iOS app on remote..."
+echo "[3/3] Building iOS app on remote..."
 
-# Fastlane
 ssh "eholtrop@$REMOTE_HOST" "
    cd $REMOTE_PATH &&
    ~/.rbenv/shims/bundle exec fastlane ios build_debug_device device_udid:$DEVICE_UDID
